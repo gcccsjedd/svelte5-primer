f6dd73426f25628dba67146d80235f07d8b9079a
Jedd Niñonuevo <jed@jasetech.ca>
2024-10-28 14:34:43 +0800

    Initial Commit with Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<main class="text-2xl p-8 bg-slate-950 text-white h-screen">
+  <div class="flex flex-col gap-4">
+    <b>Creating Svelte Project</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>npm create svelte@latest (project name)</code>
+      <code class="text-green-500">npx sv create (project name)</code>
+    </div>
+    <hr />
+    <b>Adding Tailwind CSS with zero configuration</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>npx svelte-add@latest tailwindcss</code>
+      <code class="text-green-500">npx sv add tailwindcss</code>
+    </div>
+    <hr />
+    <b>Adding Flowbite and Other packages</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>npm install -D flowbite</code>
+    </div>
+    <hr />
+    <b>Creating State Variable / Declaring State</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>let count = 0;</code>
+      <code class="text-green-500">let count = $state(0);</code>
+    </div>
+    <hr />
+    <b>Updating State</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>count++;</code>
+      <code class="text-green-500">count++;</code>
+    </div>
+    <hr />
+    <b>Computed State</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>$: doubleCount = count * 2;</code>
+      <code class="text-green-500">const doubleCount = count * 2;</code>
+    </div>
+    <hr />
+    <b>Click Event</b>
+    <div class="grid grid-cols-3 gap-4">
+      <code>{`<button on:click={() => count++}>Add</button>`}</code>
+      <code class="text-green-500"
+        >{`<button onclick={() => count++}>Add</button>`}</code
+      >
+    </div>
+  </div>
+</main>
